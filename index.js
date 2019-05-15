50e37b165ab63d008dbc36fcd0a553459f0a5abe
Bhavesh barskar <barskar.bhavesh@gmail.com>
2019-05-16 02:11:53 +0530

    made modal functioning

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var button = document.querySelectorAll(".btn-buy");
+var closeButton = document.querySelector(".modal_action2");
 console.log(button);
 for (var i = 0; i < button.length; i++) {
   button[i].addEventListener("click", function() {
     backdrop.style.display = "block";
     modal.style.display = "block";
   });
+}
+
+backdrop.addEventListener("click", close_modal);
+closeButton.addEventListener("click", close_modal);
+
+function close_modal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
 }
